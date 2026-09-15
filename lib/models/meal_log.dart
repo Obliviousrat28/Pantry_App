4f80dff319a4cf00949301f48ea1e3bfdb9664c8
Obliviousrat28 <tomwillnz@gmail.com.com>
2026-09-15 14:59:31 +1200

    Finished the merge for the add items code still need to implement data presistence and linking it to meal logging

---
+++ lib/models/meal_log.dart
@@ -0,0 +1,11 @@
+class MealLog {
+  final String mealName;
+  final String mealPrice;
+  final DateTime mealDate;
+
+  MealLog({
+    required this.mealName,
+    required this.mealPrice,
+    required this.mealDate,
+  });
+}
